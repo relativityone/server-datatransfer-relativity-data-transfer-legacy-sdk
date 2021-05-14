9d76a4500bc2eec84b3e3c94ece8f310cd99e6c4
Konrad Gałczyński <konrad.galczynski@relativity.com>
2021-05-14 09:43:30 +0200

    do not install app to workspace

--- Source/DataTransfer.Legacy.FunctionalTests/CI/Setup.cs
+++ Source/DataTransfer.Legacy.FunctionalTests/CI/Setup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Threading;
 using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
-using Relativity.Kepler.Transport;
-using Relativity.Services.Interfaces.LibraryApplication;
-using Relativity.Services.Interfaces.LibraryApplication.Models;
 using Relativity.Testing.Framework;
 using Relativity.Testing.Framework.Api;
+using Relativity.Testing.Framework.Api.Services;
 using Relativity.Testing.Framework.Configuration;
 
 namespace Relativity.DataTransfer.Legacy.FunctionalTests.CI
@@ -20,7 +15,7 @@ namespace Relativity.DataTransfer.Legacy.FunctionalTests.CI
 		[OneTimeSetUp]
 		public void SetupTests()
 		{
-			var additionalSettings = new Dictionary<string, string>
+			var additionalSettings = new Dictionary<string, string>()
 			{
 				["PerformRelativityVersionCheck"] = "false"
 			};
@@ -36,53 +31,8 @@ namespace Relativity.DataTransfer.Legacy.FunctionalTests.CI
 
 			var myRap = Path.Combine(RelativityFacade.Instance.Config.RelativityInstance.RapDirectory, "DataTransfer.Legacy.rap");
 
-			using (var applicationManager = RelativityFacade.Instance.Resolve<ILibraryApplicationManager>())
-			{
-				CreateLibraryApplicationResponse response;
-				using (var fileStream = File.OpenRead(myRap))
-				{
-					using (var keplerStream = new KeplerStream(fileStream))
-					{
-						response = applicationManager.CreateAsync(-1, keplerStream, false).GetAwaiter().GetResult();
-					}
-				}
-
-				var stopwatch = new Stopwatch();
-				stopwatch.Start();
-				while (true)
-				{
-					try
-					{
-						var installStatusResponse = applicationManager.GetLibraryInstallStatusAsync(-1, response.ApplicationIdentifier.Guids[0])
-							.GetAwaiter().GetResult();
-						if (IsInstallationCompleted(installStatusResponse))
-						{
-							break;
-						}
-
-						if (HasInstallationFailedOrTimedOut(stopwatch, installStatusResponse))
-						{
-							throw new InvalidOperationException("Failed to install application");
-						}
-
-						Thread.Sleep(TimeSpan.FromSeconds(5));
-					}
-					finally
-					{
-						stopwatch.Stop();
-					}
-				}
-			}
-		}
-
-		private static bool HasInstallationFailedOrTimedOut(Stopwatch stopwatch, IHasInstallStatus installStatusResponse)
-		{
-			return stopwatch.Elapsed > TimeSpan.FromMinutes(5) || installStatusResponse.InstallStatus.Code == InstallStatusCode.Failed;
-		}
-
-		private static bool IsInstallationCompleted(IHasInstallStatus getInstallStatusResponse)
-		{
-			return getInstallStatusResponse.InstallStatus.Code == InstallStatusCode.Completed;
+			var applicationService = RelativityFacade.Instance.Resolve<ILibraryApplicationService>();
+			applicationService.InstallToLibrary(myRap);
 		}
 	}
 }
