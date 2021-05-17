073dbbfa1f56b486004f85f17f466c57b240db9f
Paweł Horzela <pawel.horzela@relativity.com>
2021-05-17 15:28:54 +0200

    Refactor + retrieving test workspace id

--- Source/DataTransfer.Legacy.FunctionalTests/CI/WebApiCompatibility/BaseServiceCompatibilityTest.cs
+++ Source/DataTransfer.Legacy.FunctionalTests/CI/WebApiCompatibility/BaseServiceCompatibilityTest.cs
@@ -1,4 +1,8 @@
-﻿using Relativity.DataTransfer.Legacy.FunctionalTests.CI.WebApiCompatibility.Utils;
+﻿using System.Threading.Tasks;
+using Relativity.DataTransfer.Legacy.FunctionalTests.CI.WebApiCompatibility.Utils;
+using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1;
+using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1.Models;
+using Relativity.Services.Exceptions;
 
 namespace Relativity.DataTransfer.Legacy.FunctionalTests.CI.WebApiCompatibility
 {
@@ -11,6 +15,26 @@ namespace Relativity.DataTransfer.Legacy.FunctionalTests.CI.WebApiCompatibility
         {
             KeplerServiceWrapper = new KeplerServiceWrapper();
             WebApiServiceWrapper = new WebApiServiceWrapper();
+        }
+
+        protected async Task<int> GetTestWorkspaceId()
+        {
+            DataSetWrapper workspaces = null;
+            await KeplerServiceWrapper.PerformDataRequest<ICaseService>(async service =>
+            {
+                workspaces = await service.RetrieveAllEnabledAsync(string.Empty);
+            });
+
+            if (workspaces == null ||
+                workspaces.Unwrap() == null ||
+                workspaces.Unwrap().Tables.Count == 0 ||
+                workspaces.Unwrap().Tables[0].Rows.Count == 0)
+            {
+                throw new NotFoundException("Test Workspace could not be found.");
+            }
+
+            var workspace = workspaces.Unwrap().Tables[0].Rows[0].ItemArray;
+            return (int)workspace.GetValue(0);
         }
     }
 }
--- Source/DataTransfer.Legacy.FunctionalTests/CI/WebApiCompatibility/CaseServiceCompatibilityTests.cs
+++ Source/DataTransfer.Legacy.FunctionalTests/CI/WebApiCompatibility/CaseServiceCompatibilityTests.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using kCura.WinEDDS.Service;
 using NUnit.Framework;
+using Relativity.DataTransfer.Legacy.FunctionalTests.CI.WebApiCompatibility.Utils;
 using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1;
 using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1.Models;
 using Relativity.Testing.Identification;
@@ -14,7 +15,7 @@ namespace Relativity.DataTransfer.Legacy.FunctionalTests.CI.WebApiCompatibility
     public class CaseServiceCompatibilityTests : BaseServiceCompatibilityTest
     {
         [IdentifiedTest("F5312AF8-D1E1-4400-962E-43A77FE44922")]
-        public async Task ShouldGetApplicationAfterInstallation()
+        public async Task RetrieveAllEnabled_ShouldReturnTheSameResultForWebApiAndKepler()
         {
             DataSet webApiData = null;
             DataSetWrapper keplerData = null;
@@ -30,25 +31,10 @@ namespace Relativity.DataTransfer.Legacy.FunctionalTests.CI.WebApiCompatibility
                  keplerData = await service.RetrieveAllEnabledAsync(string.Empty);
             });
 
+            var id = GetTestWorkspaceId();
+
             // assert
-            AssertDataSetsAreEqual(webApiData, keplerData.Unwrap(), "");
-        }
-
-        private void AssertDataSetsAreEqual(DataSet ds1, DataSet ds2, string idColumnName)
-        {
-            Assert.NotNull(ds1);
-            Assert.NotNull(ds2);
-
-            Assert.AreEqual(1, ds1.Tables.Count);
-            Assert.AreEqual(1, ds2.Tables.Count);
-
-            var dt1 = ds1.Tables[0];
-            var dt2 = ds2.Tables[0];
-
-            Assert.AreEqual(dt1.Columns.Count, dt2.Columns.Count);
-            Assert.AreEqual(dt1.Rows.Count, dt2.Rows.Count);
-            
-            // TODO: compare data sets in a better way
+            DataSetAssertHelper.AreEqual(webApiData, keplerData.Unwrap());
         }
     }
 }
