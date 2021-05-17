d9de6e8525196b99a523b09cab2474498db951b0
Paweł Horzela <pawel.horzela@relativity.com>
2021-05-17 15:57:48 +0200

    Add missing case service tests

--- Source/DataTransfer.Legacy.FunctionalTests/CI/WebApiCompatibility/CaseServiceCompatibilityTests.cs
+++ Source/DataTransfer.Legacy.FunctionalTests/CI/WebApiCompatibility/CaseServiceCompatibilityTests.cs
@@ -26,15 +26,80 @@ namespace Relativity.DataTransfer.Legacy.FunctionalTests.CI.WebApiCompatibility
                 webApiData = caseManager.RetrieveAllEnabled();
             });
 
-            await KeplerServiceWrapper.PerformDataRequest<ICaseService>(async service => 
+            await KeplerServiceWrapper.PerformDataRequest<ICaseService>(async service =>
             {
-                 keplerData = await service.RetrieveAllEnabledAsync(string.Empty);
+                keplerData = await service.RetrieveAllEnabledAsync(string.Empty);
             });
-
-            var id = GetTestWorkspaceId();
 
             // assert
             DataSetAssertHelper.AreEqual(webApiData, keplerData.Unwrap());
+        }
+
+        [IdentifiedTest("EE590619-6890-4872-97A8-09FE90D789E3")]
+        public async Task GetAllDocumentFolderPaths_ShouldReturnTheSameResultForWebApiAndKepler()
+        {
+            string[] webApiData = null;
+            string[] keplerData = null;
+
+            WebApiServiceWrapper.PerformDataRequest((credentials, cookieContainer) =>
+            {
+                var caseManager = new CaseManager(credentials, cookieContainer);
+                webApiData = caseManager.GetAllDocumentFolderPaths();
+            });
+
+            await KeplerServiceWrapper.PerformDataRequest<ICaseService>(async service =>
+            {
+                keplerData = await service.GetAllDocumentFolderPathsAsync(string.Empty);
+            });
+
+            // assert
+            CollectionAssert.AreEqual(webApiData, keplerData);
+        }
+
+        [IdentifiedTest("")]
+        public async Task GetAllDocumentFolderPathsForCase_ShouldReturnTheSameResultForWebApiAndKepler()
+        {
+            var workspaceId = await GetTestWorkspaceId();
+            
+            string[] webApiData = null;
+            string[] keplerData = null;
+
+            WebApiServiceWrapper.PerformDataRequest((credentials, cookieContainer) =>
+            {
+                var caseManager = new CaseManager(credentials, cookieContainer);
+                webApiData = caseManager.GetAllDocumentFolderPathsForCase(workspaceId);
+            });
+
+            await KeplerServiceWrapper.PerformDataRequest<ICaseService>(async service =>
+            {
+                keplerData = await service.GetAllDocumentFolderPathsForCaseAsync(workspaceId, string.Empty);
+            });
+
+            // assert
+            CollectionAssert.AreEqual(webApiData, keplerData);
+        }
+
+        [IdentifiedTest("")]
+        public async Task Read_ShouldReturnTheSameResultForWebApiAndKepler()
+        {
+            var workspaceId = await GetTestWorkspaceId();
+
+            Relativity.DataExchange.Service.CaseInfo webApiData = null;
+            DataTransfer.Legacy.SDK.ImportExport.V1.Models.CaseInfo keplerData = null;
+
+            WebApiServiceWrapper.PerformDataRequest((credentials, cookieContainer) =>
+            {
+                var caseManager = new CaseManager(credentials, cookieContainer);
+                webApiData = caseManager.Read(workspaceId);
+            });
+
+            await KeplerServiceWrapper.PerformDataRequest<ICaseService>(async service =>
+            {
+                keplerData = await service.ReadAsync(workspaceId, string.Empty);
+            });
+
+            // assert
+            //CollectionAssert.AreEqual(webApiData, keplerData);
         }
     }
 }
