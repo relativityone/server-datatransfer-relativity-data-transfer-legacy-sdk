8b6c26507a7c4c624bfa4b49e98d289a322965b5
Paweł Horzela <pawel.horzela@relativity.com>
2021-05-19 12:26:24 +0200

    undo changes in MethodRunnerWithErrorHandling

--- Source/DataTransfer.Legacy.Services/Runners/MethodRunnerWithErrorHandling.cs
+++ Source/DataTransfer.Legacy.Services/Runners/MethodRunnerWithErrorHandling.cs
@@ -6,36 +6,36 @@ using Relativity.Services.Exceptions;
 
 namespace Relativity.DataTransfer.Legacy.Services.Runners
 {
-    public sealed class MethodRunnerWithErrorHandling : IMethodRunner
-    {
-        private readonly IMethodRunner _methodRunner;
+	public sealed class MethodRunnerWithErrorHandling : IMethodRunner
+	{
+		private readonly IMethodRunner _methodRunner;
 
-        public MethodRunnerWithErrorHandling(IMethodRunner methodRunner)
-        {
-            _methodRunner = methodRunner;
-        }
+		public MethodRunnerWithErrorHandling(IMethodRunner methodRunner)
+		{
+			_methodRunner = methodRunner;
+		}
 
-        public async Task<T> ExecuteAsync<T>(Func<Task<T>> func, int? workspaceId, string correlationId, [CallerMemberName] string callerMemberName = "")
-        {
-            try
-            {
-                return await _methodRunner.ExecuteAsync(func, workspaceId, correlationId).ConfigureAwait(false);
-            }
-            catch (SoapException soapException)
-            {
-                //todo
-                throw new ServiceException("todo", soapException);
-            }
-            catch (ServiceException serviceException)
-            {
-                //todo
-                throw new ServiceException("todo", serviceException);
-            }
-            catch (Exception exception)
-            {
-                //todo
-                throw new ServiceException("todo", exception);
-            }
-        }
-    }
+		public async Task<T> ExecuteAsync<T>(Func<Task<T>> func, int? workspaceId, string correlationId, [CallerMemberName] string callerMemberName = "")
+		{
+			try
+			{
+				return await _methodRunner.ExecuteAsync(func, workspaceId, correlationId).ConfigureAwait(false);
+			}
+			catch (SoapException soapException)
+			{
+				//todo
+				throw new ServiceException("todo", soapException);
+			}
+			catch (ServiceException serviceException)
+			{
+				//todo
+				throw new ServiceException("todo", serviceException);
+			}
+			catch (Exception exception)
+			{
+				//todo
+				throw new ServiceException("todo", exception);
+			}
+		}
+	}
 }
