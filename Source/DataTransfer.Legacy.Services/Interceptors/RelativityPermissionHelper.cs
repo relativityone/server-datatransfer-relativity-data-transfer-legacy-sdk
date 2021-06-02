3282c56e5cb713a3d2a91756d4507b25fb8c3f68
Konrad Gałczyński <konrad.galczynski@relativity.com>
2021-06-02 19:07:50 +0200

    exclude untestable code

--- Source/DataTransfer.Legacy.Services/Interceptors/RelativityPermissionHelper.cs
+++ Source/DataTransfer.Legacy.Services/Interceptors/RelativityPermissionHelper.cs
@@ -1,7 +1,9 @@
-﻿using Relativity.Core;
+﻿using System.Diagnostics.CodeAnalysis;
+using Relativity.Core;
 
 namespace Relativity.DataTransfer.Legacy.Services.Interceptors
 {
+	[ExcludeFromCodeCoverage]
 	public class RelativityPermissionHelper : IRelativityPermissionHelper
 	{
 		public bool HasAdminOperationPermission(ICoreContext coreContext, Permission permission)
