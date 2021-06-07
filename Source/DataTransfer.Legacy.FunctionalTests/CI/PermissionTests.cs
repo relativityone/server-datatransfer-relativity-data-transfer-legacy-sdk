d067ebc5698cded610da28d7dd1c531f9d367e65
Konrad Gałczyński <konrad.galczynski@relativity.com>
2021-06-07 14:37:10 +0200

    add tests L1/L3 for permissions

---
+++ Source/DataTransfer.Legacy.FunctionalTests/CI/PermissionTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1;
+using Relativity.Testing.Framework;
+using Relativity.Testing.Framework.Api;
+using Relativity.Testing.Framework.Api.Services;
+using Relativity.Testing.Framework.Models;
+using Relativity.Testing.Identification;
+using Any = TddEbook.TddToolkit.Any;
+using IUserService = Relativity.Testing.Framework.Api.Services.IUserService;
+
+namespace Relativity.DataTransfer.Legacy.FunctionalTests.CI
+{
+	[TestFixture]
+	[TestExecutionCategory.CI]
+	[TestType.Error]
+	[TestLevel.L3]
+	public class PermissionTests
+	{
+		private static Workspace _workspace;
+		private static User _user;
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			var userService = RelativityFacade.Instance.Resolve<IUserService>();
+			var groupService = RelativityFacade.Instance.Resolve<IGroupService>();
+			var permissionService = RelativityFacade.Instance.Resolve<IPermissionService>();
+			var workspaceService = RelativityFacade.Instance.Resolve<IWorkspaceService>();
+
+			SetUpNoPermissionUser(workspaceService, groupService, userService, permissionService);
+		}
+
+		private static void SetUpNoPermissionUser(IWorkspaceService workspaceService, IGroupService groupService,
+			IUserService userService, IPermissionService permissionService)
+		{
+			_workspace = workspaceService.Create(new Workspace());
+			var group = groupService.Create(new Group());
+			_user = userService.Create(new User());
+			userService.AddToGroup(_user.ArtifactID, group.ArtifactID);
+			permissionService.WorkspacePermissionService.AddWorkspaceToGroup(_workspace.ArtifactID, group.ArtifactID);
+			permissionService.WorkspacePermissionService.SetWorkspaceGroupPermissions(_workspace.ArtifactID,
+				group.ArtifactID, changeset => changeset.AdminPermissions.DisableAll());
+		}
+
+		[IdentifiedTest("5ADCC868-8E78-459E-9779-A34EBC695809")]
+		[Description("Should throw permission denied exception when user has no permissions to import/export")]
+		public async Task ShouldThrowPermissionDeniedExceptionWhenUserHasNoPermissionsToImportExport()
+		{
+			var keplerServiceFactory = RelativityFacade.Instance.GetComponent<ApiComponent>().ServiceFactory;
+			using (var caseService = keplerServiceFactory.GetServiceProxy<ICaseService>(_user.EmailAddress, _user.Password))
+			{
+				await caseService.ReadAsync(_workspace.ArtifactID, Any.String());
+			}
+		}
+	}
+}
---
+++ Source/DataTransfer.Legacy.Tests/PermissionInterceptorDecorationTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Castle.Core;
+using FluentAssertions;
+using NUnit.Framework;
+using Relativity.DataTransfer.Legacy.Services;
+using Relativity.DataTransfer.Legacy.Services.Interceptors;
+
+namespace Relativity.DataTransfer.Legacy.Tests
+{
+	[TestFixture]
+	public class PermissionInterceptorDecorationTests
+	{
+		[Test]
+		public void ShouldHaveAllServiceClassesAttributedWithPermissionInterceptor()
+		{
+			var services = typeof(BaseService).Assembly.GetTypes().Where(x => x.Name.EndsWith("Service") && !x.Name.EndsWith(nameof(BaseService)));
+			foreach (var service in services)
+			{
+				((InterceptorAttribute) service.GetCustomAttributes(typeof(InterceptorAttribute), true).First())
+					.Interceptor.ToString()
+					.Should().EndWith(nameof(PermissionCheckInterceptor));
+			}
+		}
+	}
+}
