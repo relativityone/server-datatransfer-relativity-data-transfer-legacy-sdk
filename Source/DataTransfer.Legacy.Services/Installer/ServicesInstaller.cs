eceff6dc05657f3ee4b05e7f900a1d022e8a03f8
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2021-08-24 16:36:15 +0200

    REL-581451 set `disableSignedModule` to true in Castle's `DefaultProxyFactory`
    Without that setting, Castle was creating strongly signed proxies (interceptors), which was causing `System.IO.FileLoadException: Could not load file or assembly 'Relativity.Kepler, Version=2.15.3.0, Culture=neutral, PublicKeyToken=null' or one of its dependencies. A strongly-named assembly is required.` exception when Relativity.Kepler version in runtime didn't match the build version of that dll.

--- Source/DataTransfer.Legacy.Services/Installer/ServicesInstaller.cs
+++ Source/DataTransfer.Legacy.Services/Installer/ServicesInstaller.cs
@@ -2,6 +2,7 @@
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
+using Castle.Windsor.Proxy;
 using Relativity.API;
 using Relativity.DataTransfer.Legacy.Services.Helpers;
 using Relativity.DataTransfer.Legacy.Services.Interceptors;
@@ -14,19 +15,21 @@ namespace Relativity.DataTransfer.Legacy.Services.Installer
 	{
 		public void Install(IWindsorContainer container, IConfigurationStore store)
 		{
+			container.Kernel.ProxyFactory = new DefaultProxyFactory(disableSignedModule: true);
+
 			container.Register(Component.For<IAPILog>()
 				.UsingFactoryMethod((x, c) => x.Resolve<IHelper>().GetLoggerFactory().GetLogger()));
 			container.Register(Component.For<IAPM>().Instance(Client.APMClient));
 			container.Register(Component.For<IServiceContextFactory>().ImplementedBy<ServiceContextFactory>());
 			container.Register(Component.For<ICommunicationModeStorage>().ImplementedBy<CommunicationModeInstanceSettingStorage>());
-			
+
 			// interceptors registration
 			container.Register(Component.For<LogInterceptor>().LifestyleTransient());
 			container.Register(Component.For<MetricsInterceptor>().LifestyleTransient());
 			container.Register(Component.For<PermissionCheckInterceptor>().LifestyleTransient());
 			container.Register(Component.For<UnhandledExceptionInterceptor>().LifestyleTransient());
 			container.Register(Component.For<ToggleCheckInterceptor>().LifestyleTransient());
-			
+
 			container.Register(Component.For<IMetricsPublisher>().ImplementedBy<APMMetricsPublisher>().LifestyleTransient());
 			container.Register(Component.For<IMetricsPublisher>().ImplementedBy<LoggingMetricsPublisher>().LifestyleTransient());
 			container.Register(Component.For<IMetricsContext>().ImplementedBy<MetricsContext>().LifestyleTransient());
