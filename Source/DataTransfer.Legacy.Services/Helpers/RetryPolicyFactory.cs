44b8ea530fc44d2210f6e3c1021be5d56bbbffda
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2022-03-15 13:14:27 +0000

    Pull request #65: REL-658054 added retries for deadlock exception in CodeService.CreateEncodedAsync
    Merge in DTX/relativity-data-transfer-legacy-sdk from REL-658054 to master
    
    Squashed commit of the following:
    
    commit e4763db8b1b63bd137394dcb1484d55e6ce9cfff
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Tue Mar 15 08:43:48 2022 +0100
    
        REL-658054 added retries for deadlock exception in CodeService.CreateEncodedAsync

---
+++ Source/DataTransfer.Legacy.Services/Helpers/RetryPolicyFactory.cs
@@ -0,0 +1,54 @@
+﻿using Polly;
+using Polly.Retry;
+using Relativity.API;
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Relativity.DataTransfer.Legacy.Services.Helpers
+{
+    public class RetryPolicyFactory
+    {
+        private readonly IAPILog _logger;
+
+        public RetryPolicyFactory(IAPILog logger)
+        {
+            _logger = logger;
+        }
+
+        public RetryPolicy CreateDeadlockRetryPolicy([CallerMemberName] string callerName = "")
+        {
+            const int MaxNumberOfRetries = 3;
+            const int BackoffBase = 2;
+
+            return Policy
+                .Handle<Exception>(IsDeadlockException)
+                .WaitAndRetry(MaxNumberOfRetries, GetExponentialBackoffSleepDurationProvider(BackoffBase), OnRetry);
+
+            void OnRetry(Exception ex, TimeSpan waitTime, int retryNumber, Context context)
+            {
+                _logger.LogWarning(ex, "Deadlock occured when executing '{callerName}'. Retry '{retryNumber}' out of '{maxNumberOfRetries}'. Waiting for {waitTime} before next retry attempt.",
+                    callerName,
+                    retryNumber,
+                    MaxNumberOfRetries,
+                    waitTime);
+            }
+        }
+
+        private static Func<int, TimeSpan> GetExponentialBackoffSleepDurationProvider(int backoffBase)
+        {
+            return retryNumber => TimeSpan.FromSeconds(Math.Pow(backoffBase, retryNumber));
+        }
+
+        private static bool IsDeadlockException(Exception exception)
+        {
+            var exceptions = new[]
+            {
+                exception.GetBaseException(),
+                exception,
+                exception.InnerException
+            };
+            return exceptions.Any(kCura.Data.RowDataGateway.Helper.IsDeadLock);
+        }
+    }
+}
