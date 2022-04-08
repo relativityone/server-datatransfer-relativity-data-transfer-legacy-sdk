e74d4652b408f0c58d6bd776b71f6406b5e9bd03
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2022-04-08 11:44:12 +0000

    Pull request #72: REL-658054 - RetryPolicyFactory.CreateDeadlockRetryPolicy checks if any exception in a chain was caused by deadlock.
    Merge in DTX/relativity-data-transfer-legacy-sdk from REL-658054-improved-deadlock-retry-policy to master
    
    Squashed commit of the following:
    
    commit a187f36865b1242ba1fc24cfea67ee49953bdfa1
    Author: Konrad Kopciuch <konrad.kopciuch@relativity.com>
    Date:   Fri Apr 8 12:47:32 2022 +0200
    
        REL-658054 - RetryPolicyFactory.CreateDeadlockRetryPolicy checks if any exception in a chain was caused by deadlock.

--- Source/DataTransfer.Legacy.Services/Helpers/RetryPolicyFactory.cs
+++ Source/DataTransfer.Legacy.Services/Helpers/RetryPolicyFactory.cs
@@ -2,6 +2,7 @@
 using Polly.Retry;
 using Relativity.API;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
@@ -9,6 +10,9 @@ namespace Relativity.DataTransfer.Legacy.Services.Helpers
 {
     public class RetryPolicyFactory
     {
+        private const int DefaultMaxNumberOfRetries = 3;
+        private const int DefaultBackoffBase = 2;
+
         private readonly IAPILog _logger;
 
         public RetryPolicyFactory(IAPILog logger)
@@ -18,19 +22,21 @@ namespace Relativity.DataTransfer.Legacy.Services.Helpers
 
         public RetryPolicy CreateDeadlockRetryPolicy([CallerMemberName] string callerName = "")
         {
-            const int MaxNumberOfRetries = 3;
-            const int BackoffBase = 2;
+            return CreateDeadlockRetryPolicy(DefaultMaxNumberOfRetries, DefaultBackoffBase, callerName);
+        }
 
+        public RetryPolicy CreateDeadlockRetryPolicy(int maxNumberOfRetries, int backoffBase, [CallerMemberName] string callerName = "")
+        {
             return Policy
-                .Handle<Exception>(IsDeadlockException)
-                .WaitAndRetry(MaxNumberOfRetries, GetExponentialBackoffSleepDurationProvider(BackoffBase), OnRetry);
+    .Handle<Exception>(IsDeadlockException)
+    .WaitAndRetry(maxNumberOfRetries, GetExponentialBackoffSleepDurationProvider(backoffBase), OnRetry);
 
             void OnRetry(Exception ex, TimeSpan waitTime, int retryNumber, Context context)
             {
                 _logger.LogWarning(ex, "Deadlock occured when executing '{callerName}'. Retry '{retryNumber}' out of '{maxNumberOfRetries}'. Waiting for {waitTime} before next retry attempt.",
                     callerName,
                     retryNumber,
-                    MaxNumberOfRetries,
+                    maxNumberOfRetries,
                     waitTime);
             }
         }
@@ -40,15 +46,30 @@ namespace Relativity.DataTransfer.Legacy.Services.Helpers
             return retryNumber => TimeSpan.FromSeconds(Math.Pow(backoffBase, retryNumber));
         }
 
-        private static bool IsDeadlockException(Exception exception)
+        private bool IsDeadlockException(Exception exception)
         {
-            var exceptions = new[]
+            Exception lastException = exception;
+            var exceptions = GetAllExceptionsInChain(exception);
+            bool isDeadlock = exceptions.Any(kCura.Data.RowDataGateway.Helper.IsDeadLock);
+
+            _logger.LogWarning(exception, "Will not retry - exception was not caused by SQL deadlock.");
+
+            return isDeadlock;
+        }
+
+        private static IEnumerable<Exception> GetAllExceptionsInChain(Exception exception)
+        {
+            const int MaxChainLength = 100;
+
+            int chainLength = 0;
+            Exception currentException = exception;
+            while (currentException != null && chainLength < MaxChainLength)
             {
-                exception.GetBaseException(),
-                exception,
-                exception.InnerException
-            };
-            return exceptions.Any(kCura.Data.RowDataGateway.Helper.IsDeadLock);
+                yield return currentException;
+
+                chainLength++;
+                currentException = currentException.InnerException;
+            }
         }
     }
 }
