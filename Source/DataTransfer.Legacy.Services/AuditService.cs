b0d9b436a152d4a11d3dcfdd77923b772f835a31
Dominik Tomasiewicz <dominik.tomasiewicz@relativity.com>
2022-05-16 10:23:26 +0000

    REL-857917: backported REL-669697

--- Source/DataTransfer.Legacy.Services/AuditService.cs
+++ Source/DataTransfer.Legacy.Services/AuditService.cs
@@ -10,9 +10,9 @@ namespace Relativity.DataTransfer.Legacy.Services
 {
 	[Interceptor(typeof(UnhandledExceptionInterceptor))]
 	[Interceptor(typeof(ToggleCheckInterceptor))]
-	[Interceptor(typeof(PermissionCheckInterceptor))]
 	[Interceptor(typeof(LogInterceptor))]
 	[Interceptor(typeof(MetricsInterceptor))]
+	[Interceptor(typeof(PermissionCheckInterceptor))]
 	public class AuditService : BaseService, IAuditService
 	{
 		private readonly IMassExportManager _massExportManager;
--- Source/DataTransfer.Legacy.Services/CodeService.cs
+++ Source/DataTransfer.Legacy.Services/CodeService.cs
@@ -13,9 +13,9 @@ namespace Relativity.DataTransfer.Legacy.Services
 {
 	[Interceptor(typeof(UnhandledExceptionInterceptor))]
 	[Interceptor(typeof(ToggleCheckInterceptor))]
-	[Interceptor(typeof(PermissionCheckInterceptor))]
 	[Interceptor(typeof(LogInterceptor))]
 	[Interceptor(typeof(MetricsInterceptor))]
+	[Interceptor(typeof(PermissionCheckInterceptor))]
 	public class CodeService : BaseService, ICodeService
 	{
 		private readonly CodeManagerImplementation _codeManager;
