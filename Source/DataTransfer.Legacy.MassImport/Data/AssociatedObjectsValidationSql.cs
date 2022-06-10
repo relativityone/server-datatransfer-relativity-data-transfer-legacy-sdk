3ec6acc0376deaa6ae283d262d59a97b8d7c3f35
Michał Wojarski <michal.wojarski@relativity.com>
2022-06-10 09:01:06 +0000

    Pull request #84: REL-691656 bad associative object message 2
    Merge in DTX/relativity-data-transfer-legacy-sdk from REL-691656-bad-associative-object-message-2 to master
    
    * commit 'b82c8458be3f3d0f41e02ca0c7ad113f91f551ef':
      REL-691656 bump version
      REL-691656 improve error message
      REL-691656 add field to error message
      REL-691656 move ImportStatusHelper and other model classes to DataTransfer.Legacy

--- Source/DataTransfer.Legacy.MassImport/Data/AssociatedObjectsValidationSql.cs
+++ Source/DataTransfer.Legacy.MassImport/Data/AssociatedObjectsValidationSql.cs
@@ -44,7 +44,8 @@ WHERE
 			return $@"/*create errors for associated objects that do not exist*/
 UPDATE N
 SET
-	[kCura_Import_Status] = [kCura_Import_Status] + {(long)Relativity.MassImport.DTO.ImportStatus.ErrorAssociatedObjectIsMissing}
+	[kCura_Import_Status] = [kCura_Import_Status] + {(long)Relativity.MassImport.DTO.ImportStatus.ErrorAssociatedObjectIsMissing},
+	[kCura_Import_ErrorData] = '{field.DisplayName}|' + N.[{field.GetColumnName()}] + '|{associatedObjectTable}'
 FROM [Resource].[{tableNames.Native}] N
 WHERE
 	N.[{field.GetColumnName()}] IS NOT NULL
