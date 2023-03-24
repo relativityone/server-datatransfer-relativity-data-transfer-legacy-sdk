b77162e9ca8166a6afed36c0194ee3131f2f71c9
Konrad Kopciuch <konrad.kopciuch@relativity.com>
2023-03-24 18:16:03 +0100

    REL-797147 reduce chunk size when lenght of serialized data is too large to be deserialized

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
+## [0.3.21]
+- REL-797147 - reduce chunk size when lenght of serialized data is too large to be deserialized
+
 ## [0.3.20]
 - REL-829772 - Send event with number of empty texts passed to DataGrid
 
--- Source/DataTransfer.Legacy.Services/ExportService.cs
+++ Source/DataTransfer.Legacy.Services/ExportService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Castle.Core;
+using Relativity.API;
 using Relativity.Core;
 using Relativity.Core.Exception;
 using Relativity.DataTransfer.Legacy.SDK.ImportExport.V1;
@@ -21,8 +23,12 @@ namespace Relativity.DataTransfer.Legacy.Services
 	public class ExportService : BaseService, IExportService
 	{
 		private static readonly string[] DynamicallyLoadedDllPaths = { Config.DynamicallyLoadedStandardSearchDLLs, Config.DynamicallyLoadedClientSearchDLLs };
+		private readonly IAPILog _logger;
 
-		public ExportService(IServiceContextFactory serviceContextFactory) : base(serviceContextFactory) { }
+		public ExportService(IServiceContextFactory serviceContextFactory, IAPILog logger) : base(serviceContextFactory)
+		{
+			_logger = logger;
+		}
 
 		public Task<InitializationResults> InitializeSearchExportAsync(int workspaceID, int searchArtifactID, int[] avfIDs, int startAtRecord, string correlationID)
 		{
@@ -99,7 +105,30 @@ namespace Relativity.DataTransfer.Legacy.Services
 				result = export.RetrieveResultsBlockStartingFromIndex(GetBaseServiceContext(workspaceID), runID, avfIds, chunkSize, displayMulticodesAsNested, multiValueDelimiter, nestedValueDelimiter, index);
 			}
 
-			return new ExportDataWrapper(result);
+			return WrapResults(result);
+		}
+
+		private ExportDataWrapper WrapResults(object[] result)
+		{
+			// REL-797147, System.OutOfMemoryException: Array dimensions exceeded supported range
+			const int maximumSerializedDataLength = 456176861; // 1024 rows, 1024 columns, 384 characters per each element
+
+			var dataWrapper = new ExportDataWrapper(result);
+			while (dataWrapper.SerializedDataLength > maximumSerializedDataLength && result.Length > 1)
+			{
+				int newResultsLength = result.Length / 2;
+				_logger.LogWarning(
+					"Length of ExportDataWrapper is too large: '{length}'. Reducing size of result set from {length} to {newLength} records.",
+					dataWrapper.SerializedDataLength,
+					result.Length,
+					newResultsLength);
+
+				dataWrapper = null;
+				result = result.Take(newResultsLength).ToArray();
+				dataWrapper = new ExportDataWrapper(result);
+			}
+
+			return dataWrapper;
 		}
 
 		public Task<bool> HasExportPermissionsAsync(int workspaceID, string correlationID)
