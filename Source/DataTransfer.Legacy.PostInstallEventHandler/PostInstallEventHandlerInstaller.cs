92628f9032b24ffad9d73d0de0c2606445e4de5e
Mateusz Kulik <mateusz.kulik@relativity.com>
2023-05-11 14:19:05 +0200

    v3

--- Source/DataTransfer.Legacy.PostInstallEventHandler/PostInstallEventHandlerInstaller.cs
+++ Source/DataTransfer.Legacy.PostInstallEventHandler/PostInstallEventHandlerInstaller.cs
@@ -16,27 +16,17 @@ namespace Relativity.DataTransfer.Legacy.PostInstallEventHandler
 		public static IWindsorContainer CreateContainer(Func<IHelper> helperFactory)
 		{
 			const bool manageExternally = true;
-			IHelper iHelper = null;
 
 			var container = new WindsorContainer();
-			try
-			{
-				container.Register(Component.For<IHelper>().UsingFactoryMethod(helperFactory, manageExternally)
+			container.Register(Component.For<IHelper>().UsingFactoryMethod(helperFactory, manageExternally)
 					.LifestyleTransient());
-				iHelper = container.Resolve<IHelper>();
-				container.Register(Component.For<IRetryPolicyProvider>().ImplementedBy<RetryPolicyProvider>()
+			container.Register(Component.For<IRetryPolicyProvider>().ImplementedBy<RetryPolicyProvider>()
 					.LifestyleTransient());
-				container.Register(Component.For<IInstanceSettingsService>().ImplementedBy<InstanceSettingsService>()
+			container.Register(Component.For<IInstanceSettingsService>().ImplementedBy<InstanceSettingsService>()
 					.LifestyleTransient());
-				container.Register(Component.For<IAPILog>()
-					.UsingFactoryMethod(k => iHelper.GetLoggerFactory().GetLogger()).LifestyleTransient());
-				return container;
-			}
-			finally
-			{
-				container.Release(iHelper);
-			}
-			
+			container.Register(Component.For<IAPILog>()
+					.UsingFactoryMethod(k => helperFactory().GetLoggerFactory().GetLogger()).LifestyleTransient());
+			return container;
 		}
 	}
 }
