bd28e11f4c7bbbde089b15eb6a5eaa65cc21099b
Michał Wojarski <michal.wojarski@relativity.com>
2023-05-23 14:24:08 +0200

    REL-8471710 add DisableCALForDataGridToggle

---
+++ Source/DataTransfer.Legacy.MassImport/Toggles/DisableCALForDataGridToggle.cs
@@ -0,0 +1,11 @@
+﻿namespace DataTransfer.Legacy.MassImport.Toggles
+{
+	using Relativity.Toggles;
+
+	[Description("Disable CAL usage in Data Grid", "")]
+	[DefaultValue(false)]
+	[ExpectedRemovalDate(2024, 1, 1)]
+	public class DisableCALForDataGridToggle : IToggle
+	{
+	}
+}
