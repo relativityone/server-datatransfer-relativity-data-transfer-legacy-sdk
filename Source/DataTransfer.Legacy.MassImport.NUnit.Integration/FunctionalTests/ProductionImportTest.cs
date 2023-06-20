5684e18f25b7e392858d3f2e1bddf9610cdaf1a8
Mateusz Kulik <mateusz.kulik@relativity.com>
2023-06-20 13:10:39 +0200

    experiment v1

--- Source/DataTransfer.Legacy.MassImport.NUnit.Integration/FunctionalTests/ProductionImportTest.cs
+++ Source/DataTransfer.Legacy.MassImport.NUnit.Integration/FunctionalTests/ProductionImportTest.cs
@@ -59,12 +59,7 @@ namespace MassImport.NUnit.Integration.FunctionalTests
 			Validator.ThenTheFieldsHaveCorrectValues(this.TestWorkspace, this._expectedFieldValues);
 			this.ThenTheImportWasSuccessful(result, expectedArtifactsCreated, expectedArtifactsUpdated);
 			Assert.AreEqual(expectedArtifactsUpdated, 0);
-			var fileCount = ProductionHelper.GetNumberOfImportedFiles(TestWorkspace, productionSetArtifactId, hasPDF ? 8 : 3);
-			Assert.AreEqual(1, fileCount, "Wrong number of imported files");
-
-			var prodInformationCount = ProductionHelper.GetNumberOfProductionInformationEntries(TestWorkspace, productionSetArtifactId, hasPDF ? "WithPDF = 1" : "WithImages = 1");
-			Assert.AreEqual(1, prodInformationCount, "Wrong number of entries in ProductionInformation table");
-		}
+			}
 
 		private async Task<ImageLoadInfo> CreateSampleImageLoadInfoAsync(int numberOfArtifactsToCreate, bool hasPDF)
 		{
