bbbd28130fb655a95a6375d06a4a709d1bb1970a
Dawid Stoga <dawid.stoga@relativity.com>
2023-11-17 12:00:41 +0100

    Add DisableRdcAndImportApiToggle

---
+++ Source/DataTransfer.Legacy.Services/Toggles/DisableRdcAndImportApiToggle.cs
@@ -0,0 +1,10 @@
+﻿using Relativity.Toggles;
+
+namespace Relativity.DataTransfer.Legacy.Services.Toggles
+{
+	[Description("Disables Relativity Desktop Client (RDC) and Import API (IAPI).", "Import Export API")]
+	[DefaultValue(false)]
+	public class DisableRdcAndImportApiToggle : IToggle
+	{
+	}
+}
