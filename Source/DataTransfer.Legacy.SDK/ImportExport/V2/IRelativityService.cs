8def6b34af09ac766c7605fa50309701fb61e5a8
Dawid Stoga <dawid.stoga@relativity.com>
2023-11-17 11:55:25 +0100

    Add RelativityService V2

---
+++ Source/DataTransfer.Legacy.SDK/ImportExport/V2/IRelativityService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Relativity.DataTransfer.Legacy.SDK.ImportExport.V2.Models;
+using Relativity.Kepler.Services;
+
+namespace Relativity.DataTransfer.Legacy.SDK.ImportExport.V2
+{
+	[WebService("Relativity Manager replacement Service")]
+	[ServiceAudience(Audience.Private)]
+	[RoutePrefix("relativity")]
+	public interface IRelativityService : IDisposable
+	{
+		[HttpPost]
+		[Route("RetrieveCurrencySymbolAsync")]
+		Task<string> RetrieveCurrencySymbolAsync(string correlationID);
+
+		
+		[HttpPost]
+		[Route("IsImportEmailNotificationEnabledAsync")]
+		Task<bool> IsImportEmailNotificationEnabledAsync(string correlationID);
+
+		[HttpPost]
+		[Route("RetrieveRdcConfigurationAsync")]
+		Task<DataSetWrapper> RetrieveRdcConfigurationAsync(string correlationID);
+	}
+}
---
+++ Source/DataTransfer.Legacy.Services/V2/RelativityService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Castle.Core;
+using Relativity.Core;
+using Relativity.Core.Service;
+using Relativity.DataTransfer.Legacy.SDK.ImportExport.V2;
+using Relativity.DataTransfer.Legacy.SDK.ImportExport.V2.Models;
+using Relativity.DataTransfer.Legacy.Services.Helpers;
+using Relativity.DataTransfer.Legacy.Services.Interceptors;
+
+namespace Relativity.DataTransfer.Legacy.Services.V2
+{
+	[Interceptor(typeof(UnhandledExceptionInterceptor))]
+	[Interceptor(typeof(ToggleCheckInterceptor))]
+	[Interceptor(typeof(LogInterceptor))]
+	[Interceptor(typeof(MetricsInterceptor))]
+	[Interceptor(typeof(PermissionCheckInterceptor))]
+	[Interceptor(typeof(DistributedTracingInterceptor))]
+	public class RelativityService : BaseService, IRelativityService
+	{
+		public RelativityService(IServiceContextFactory serviceContextFactory) : base(serviceContextFactory) { }
+
+		public Task<string> RetrieveCurrencySymbolAsync(string correlationID)
+		{
+			var result = CultureInfo.CurrentCulture.NumberFormat.CurrencySymbol;
+			return Task.FromResult(result);
+		}
+
+		public Task<bool> IsImportEmailNotificationEnabledAsync(string correlationID)
+		{
+			var result = Config.SendNotificationOnImportCompletion;
+			return Task.FromResult(result);
+		}
+
+		public Task<DataSetWrapper> RetrieveRdcConfigurationAsync(string correlationID)
+		{
+
+			var result = WebAPIHelper.RetrieveRdcConfiguration(GetBaseServiceContext(AdminWorkspace));
+			return Task.FromResult(result != null ? new DataSetWrapper(result.ToDataSet()) : null);
+		}
+	}
+}
